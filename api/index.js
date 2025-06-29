ef54e830a1560bdbff4533b1bdcff3ce22b11f5e
Mahmoud Mostafa <Mmostafa1999>
2025-06-29 20:04:16 +0300

    Fix: use dynamic port for Render

--- api/index.js
+++ api/index.js
@@ -60,7 +60,8 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(8800, () => {
-  connect();
-  console.log("Server is running on port 8800");
+const PORT = process.env.PORT || 8800;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
